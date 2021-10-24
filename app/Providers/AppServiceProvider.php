d219511fe193a148d151d7a147faf0dfb85a593d
tkinoshita <1bey1140@gmail.com>
2021-10-24 22:19:13 +0900

    HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\LineMessagingApiService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // インスタンス生成時、access_tokenやchannel_secretを設定
+        $this->app->bind(LineMessagingApiService::class, function () {
+            return new LineMessagingApiService(
+                config('line.access_token'), config('line.channel_secret')
+            );
+        });
     }
 
     /**
@@ -23,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
